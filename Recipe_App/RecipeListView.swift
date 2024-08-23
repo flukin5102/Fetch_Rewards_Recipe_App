45734adba1dd791091b8a4abd32d0cf44ffe0bef
Sungwoo Lee <lsungwoo0916@gmail.com>
2024-08-23 01:04:38 -0400

    Added search/filter logic and made minor design changes

--- Recipe_App/RecipeListView.swift
+++ Recipe_App/RecipeListView.swift
@@ -14,15 +14,24 @@ enum NavStack: Hashable {
 
 struct RecipeListView: View {
     @State var showView = false
-    @State var recipeThumbnails: [RecipeThumbnailModel] = []
     @State var searchQuery = ""
     @State var navPath = NavigationPath()
+    
+    @State var recipeThumbnails: [RecipeThumbnailModel] = []
+    var recs: [RecipeThumbnailModel] {
+        if searchQuery.isEmpty {
+            return recipeThumbnails
+        } else {
+            return recipeThumbnails.filter { $0.name.localizedCaseInsensitiveContains(searchQuery)}
+        }
+    }
     
     var body: some View {
         NavigationStack(path: $navPath) {
             if showView {
-                List($recipeThumbnails) { $recipeThumbnail in
+                List(recs) { recipeThumbnail in
                     RecipeThumbnailView(recipeThumbnail: recipeThumbnail, navPath: $navPath)
+                        .listRowInsets(.init(top: 0, leading: 0, bottom: 24, trailing: 0))
                 }
                 .listStyle(PlainListStyle())
                 .searchable(text: $searchQuery, placement: .toolbar, prompt: "Search")
@@ -38,14 +47,13 @@ struct RecipeListView: View {
                 ProgressView()
                     .scaleEffect(2)
                     .task {
-                        recipeThumbnails = await getRecipes()
-                        showView = recipeThumbnails.count > 0
+                        await getRecipes()
                     }
             }
         }
     }
     
-    func getRecipes() async -> [RecipeThumbnailModel] {
+    func getRecipes() async {
         let backendCheckoutUrl = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")!
         var request = URLRequest(url: backendCheckoutUrl)
         request.httpMethod = "POST"
@@ -55,12 +63,12 @@ struct RecipeListView: View {
             let (data, _) = try await URLSession.shared.data(for: request)
             let json = try JSONSerialization.jsonObject(with: data) as? [String:[[String: Any]]] ?? [:]
             
-            
-            let recipes = json["meals"]?.compactMap ({ RecipeThumbnailModel(id: $0["idMeal"] as! String, name: $0["strMeal"] as! String, imageURL: $0["strMealThumb"] as! String) })
-            return recipes ?? []
+            var recipes = json["meals"]?.compactMap ({ RecipeThumbnailModel(id: $0["idMeal"] as! String, name: $0["strMeal"] as! String, imageURL: $0["strMealThumb"] as! String) }) ?? []
+            recipes.sort(by: { $0.name < $1.name }) //sort alphabetically
+            recipeThumbnails = recipes
+            showView = recipeThumbnails.count > 0
         } catch {
             print("GetRecipeThumbnails Error: \(error)")
-            return []
         }
     }
 }
@@ -78,9 +86,12 @@ struct RecipeThumbnailView: View {
             WebImage(url: URL(string: recipeThumbnail.imageURL))
                 .resizable()
                 .scaledToFill()
-                .frame(height: 300, alignment: .center)
+                .frame(height: 400, alignment: .center)
                 .clipped()
             Text(recipeThumbnail.name)
+                .bold()
+                .font(.title2)
+                .padding(.horizontal, 8)
         }
         .onTapGesture {
             navPath.append(NavStack.DetailedView(recipeThumbnail))
